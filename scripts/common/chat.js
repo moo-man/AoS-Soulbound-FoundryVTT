d5b87643b55356b4a6c66de37204c13a9fb0e30d
Marco <marco.griesel@gmail.com>
2021-06-28 00:52:59 +0200

    Added multiplier for backstab

--- scripts/common/chat.js
+++ scripts/common/chat.js
@@ -11,16 +11,24 @@ export const addChatMessageContextOptions = function (html, options) {
         return message.isRoll
             && message.isContentVisible //can be seen
             && canvas.tokens?.controlled.length //has something selected
-            && message.find('.damage-value').length; //is damage roll
+            && li.find('.damage-value').length; //is damage roll
     };
     options.push(
         {
             name: game.i18n.localize("CHAT.APPLY_DAMAGE"),
             icon: '<i class="fas fa-user-minus"></i>',
             condition: canApply,
-            callback: li => applyChatCardDamage(li)
+            callback: li => applyChatCardDamage(li, 1);
         }
     );
+	options.push(
+		{
+            name: game.i18n.localize("CHAT.APPLY_DOUBLE_DAMAGE"),
+            icon: '<i class="fas fa-user-minus"></i>',
+            condition: canApply,
+            callback: li => applyChatCardDamage(li, 2);
+		}
+	);
     return options;
 };
 
@@ -28,10 +36,11 @@ export const addChatMessageContextOptions = function (html, options) {
  * @param {HTMLElement} messsage    The chat entry which contains the roll data
  * @return {Promise}
  */
-function applyChatCardDamage(message) {    
+function applyChatCardDamage(message, multiplier) {    
     let el = message.find('.damage-value');
     let regex = /\d+/;
     let damage = parseInt($(el[0]).text().match(regex));
+	damage *= multiplier;
     // apply to any selected actors
     return Promise.all(canvas.tokens.controlled.map(t => {
         const a = t.actor;
