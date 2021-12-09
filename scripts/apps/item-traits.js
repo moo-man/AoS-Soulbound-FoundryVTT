732fbfa735d721d15bcc5f501140cd090015249a
Russell <russellthurman94@gmail.com>
2021-12-09 16:09:23 -0600

    migration fixes

--- scripts/apps/item-traits.js
+++ scripts/apps/item-traits.js
@@ -1,46 +1,50 @@
-export default class ItemTraits extends FormApplication 
-{
+export default class ItemTraits extends FormApplication {
     static get defaultOptions() {
         return mergeObject(super.defaultOptions, {
             id: "item-traits",
-            template : "systems/age-of-sigmar-soulbound/template/apps/item-traits.html",
-            height : "auto",
-            width : "auto",
-            title : "Item Traits"
-            
+            template: "systems/age-of-sigmar-soulbound/template/apps/item-traits.html",
+            height: "auto",
+            width: "auto",
+            title: "Item Traits"
+
         })
     }
 
     getData() {
         let data = super.getData();
         data.traits = Object.keys(game.aos.config.traits).map(i => {
-            let existing = this.object.traits.find(t => t.name == i)
-            return  {
-                display : game.aos.config.traits[i],
-                key : i,
-                existingTrait : existing,
-                hasValue : game.aos.config.traitsWithValue.includes(i),
+            try {
+                let existing = this.object.traits.find(t => t.name == i)
+                return {
+                    display: game.aos.config.traits[i],
+                    key: i,
+                    existingTrait: existing,
+                    hasValue: game.aos.config.traitsWithValue.includes(i),
+                }
+            }
+            catch (e) {
+                return {
+                    display: game.aos.config.traits[i],
+                    key: i,
+                    hasValue: game.aos.config.traitsWithValue.includes(i),
+                }
             }
         })
-        
-        return data;
+        return data
     }
 
 
-    _updateObject(event, formData)
-    {
+    _updateObject(event, formData) {
         let newTraits = []
-        for (let key in formData)
-        {
-            if (formData[key] && !key.includes("value"))
-            {
-                let traitObj = { name : key}
+        for (let key in formData) {
+            if (formData[key] && !key.includes("value")) {
+                let traitObj = { name: key }
                 if (formData[`${key}-value`])
                     traitObj.value = formData[`${key}-value`]
                 newTraits.push(traitObj)
             }
         }
-        this.object.update({"data.traits" : newTraits})
+        this.object.update({ "data.traits": newTraits })
     }
 
     activateListeners(html) {
