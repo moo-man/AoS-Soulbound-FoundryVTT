11d043a5dd0e524a4cc3b1310735e976ca8f3f5d
Marco <bla@mad.com>
2022-01-21 16:39:13 +0100

    Fix Aetheric Device not used as Attacl as Hotbar Macro

--- scripts/system/macro.js
+++ scripts/system/macro.js
@@ -19,7 +19,7 @@ export default class AOS_MacroUtil {
         if (!item) return ui.notifications.warn(`${game.i18n.localize("NOTIFICATION.MACRO_ITEM_NOT_FOUND")} ${itemName}`);
 
         // Trigger the item roll
-        if(item.isWeapon) {        
+        if(item.isAttack) { // Is Weapon or Aetheric Device with Damage       
             return AOS_MacroUtil.createWeaponDialog(actor, item);        
         }
         if(item.isSpell) {
